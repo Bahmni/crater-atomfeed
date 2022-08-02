061b8529ca544c05c691e91adfa5bdc955284b54
stavtnameh <hemanth.v@beehyv.com>
2022-08-02 15:28:16 +0530

    BAH-1938| changed base image to tomcat

--- package/docker/scripts/start.sh
+++ package/docker/scripts/start.sh
@@ -2,15 +2,15 @@
 set -e
 
 echo "[INFO] Substituting Environment Variables"
-envsubst < /$CATALINA_HOME/atomfeed.properties.template > $CATALINA_HOME/WEB-INF/classes/atomfeed.properties
-envsubst < /$CATALINA_HOME/application.properties.template > /$CATALINA_HOME/WEB-INF/classes/application.properties
-envsubst < /$CATALINA_HOME/crater.properties.template > /$CATALINA_HOME/WEB-INF/classes/crater.properties
+envsubst < /usr/local/tomcat/war/atomfeed.properties.template > /usr/local/tomcat/war/WEB-INF/classes/atomfeed.properties
+envsubst < /usr/local/tomcat/war/application.properties.template > /usr/local/tomcat/war/WEB-INF/classes/application.properties
+envsubst < /usr/local/tomcat/war/crater.properties.template > /usr/local/tomcat/war/WEB-INF/classes/crater.properties
 echo "-----contents of atomfeed.properties-----"
-cat $CATALINA_HOME/WEB-INF/classes/atomfeed.properties
+cat /usr/local/tomcat/war/WEB-INF/classes/atomfeed.properties
 echo "-----contents of application.properties-----"
-cat $CATALINA_HOME/WEB-INF/classes/application.properties
+cat /usr/local/tomcat/war/WEB-INF/classes/application.properties
 echo "-----contents of crater.properties------"
-cat "$CATALINA_HOME/WEB-INF/classes/crater.properties"
+cat /usr/local/tomcat/war/WEB-INF/classes/crater.properties
 echo "---------------------------------------------"
 echo "Waiting for ${CRATER_DB_HOST}.."
 sh wait-for.sh -t 300 "${CRATER_DB_HOST}":"${CRATER_DB_PORT}"
@@ -18,12 +18,9 @@ sh wait-for.sh -t 300 "${CRATER_DB_HOST}":"${CRATER_DB_PORT}"
 echo "Waiting for ${OPENMRS_HOST}.."
 sh wait-for.sh -t 300 "${OPENMRS_HOST}":"${OPENMRS_PORT}"
 
-rm crater-atomfeed.war
-jar cvf crater-atomfeed.war *
-mv crater-atomfeed.war webapps
+# rm crater-atomfeed.war
+# jar cvf crater-atomfeed.war *
+# mv crater-atomfeed.war webapps
 echo "[INFO] Starting Application"
-pwd
-ls
-ls WEB-INF
-# java -jar $SERVER_OPTS $DEBUG_OPTS /$CATALINA_HOME/crater.$CATALINA_HOME
-$CATALINA_HOME/bin/catalina.sh run
+# java -jar $SERVER_OPTS $DEBUG_OPTS /usr/local/tomcat/crater.usr/local/tomcat
+usr/local/tomcat/bin/catalina.sh run
