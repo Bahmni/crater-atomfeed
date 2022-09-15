3a5912dce98f7fb171e69cd7aa7f9849044bfb1c
Tuhin <tuhin.s@beehyv.com>
2022-09-15 18:14:18 +0530

    BAH-1938|Dockerized crater-atomfeed

---
+++ package/docker/scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+echo "[INFO] Substituting Environment Variables"
+envsubst < /opt/crater-atomfeed/etc/atomfeed.properties.template > ${HOME}/atomfeed.properties
+envsubst < /opt/crater-atomfeed/etc/application.properties.template > ${HOME}/application.properties
+envsubst < /opt/crater-atomfeed/etc/crater.properties.template > ${HOME}/crater.properties
+
+echo "Waiting for ${CRATER_ATOMFEED_DB_HOST}.."
+sh wait-for.sh -t 300 "${CRATER_ATOMFEED_DB_HOST}":"${CRATER_ATOMFEED_DB_PORT}"
+
+echo "Waiting for ${OPENMRS_HOST}.."
+sh wait-for.sh -t 3600 "${OPENMRS_HOST}":"${OPENMRS_PORT}"
+
+echo "[INFO] Starting Application"
+java -jar $SERVER_OPTS $DEBUG_OPTS /opt/crater-atomfeed/lib/crater-atomfeed.jar
