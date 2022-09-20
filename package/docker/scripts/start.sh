5fda20111f19e50787f2c7dba0d9f48bfdecffed
stavtnameh <hemanth.v@beehyv.com>
2022-09-20 13:17:17 +0530

    BAH-1938|Refactored files for removing db.properties

--- package/docker/scripts/start.sh
+++ package/docker/scripts/start.sh
@@ -5,7 +5,6 @@ echo "[INFO] Substituting Environment Variables"
 envsubst < /opt/crater-atomfeed/etc/atomfeed.properties.template > ${HOME}/atomfeed.properties
 envsubst < /opt/crater-atomfeed/etc/application.properties.template > ${HOME}/application.properties
 envsubst < /opt/crater-atomfeed/etc/crater.properties.template > ${HOME}/crater.properties
-envsubst < /opt/crater-atomfeed/etc/db.properties.template > ${HOME}/db.properties
 
 echo "Waiting for ${CRATER_ATOMFEED_DB_HOST}.."
 sh wait-for.sh -t 300 "${CRATER_ATOMFEED_DB_HOST}":"${CRATER_ATOMFEED_DB_PORT}"
