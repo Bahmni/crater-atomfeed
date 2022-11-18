da6912052d78bbd4472e6e97ddbc3233dd0b5dc4
atish160384 <atish@beehyv.com>
2022-11-18 11:59:03 +0530

    BAH-2405 | Refactor. addParameters through List Method

--- feed-integration-webapp/src/main/java/org/bahmni/module/feedintegration/crater/CraterLogin.java
+++ feed-integration-webapp/src/main/java/org/bahmni/module/feedintegration/crater/CraterLogin.java
@@ -1,22 +1,29 @@
 package org.bahmni.module.feedintegration.crater;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Component
 @Scope("singleton")
@@ -46,26 +53,32 @@ public class CraterLogin {
         try {
             CloseableHttpResponse response;
             httpclient = HttpClients.createDefault();
-            HttpUriRequest httppost = RequestBuilder.post()
-                    .setUri(new URI(crater_url + "/api/v1/auth/login"))
-                    .addParameter("username", username)
-                    .addParameter("password", password)
-                    .addParameter("device_name", device_name)
-                    .build();
-
-            httppost.addHeader("Content Type", "*/*");
-            httppost.addHeader("Accept", "*/*");
-            httppost.addHeader("company", company);
-
-            response = httpclient.execute(httppost);
+            
+            List nameValuePairs = new ArrayList();
+    	    nameValuePairs.add(new BasicNameValuePair("username", username));
+    	    nameValuePairs.add(new BasicNameValuePair("password", password));
+    	    nameValuePairs.add(new BasicNameValuePair("device_name", device_name));
+     	   
+    	    HttpPost httpPost = new HttpPost(crater_url + "/api/v1/auth/login");
+    	    URI uri = new URIBuilder(httpPost.getURI())
+    	      .setParameters(nameValuePairs)
+    	      .build();
+    	   ((HttpRequestBase) httpPost).setURI(uri);
+    	   httpPost.addHeader("Content-Type", "*/*");
+           httpPost.addHeader("Accept", "*/*");
+           httpPost.addHeader("company", company);
+           response = httpclient.execute(httpPost);
             String authString = EntityUtils.toString(response.getEntity());
             JSONObject authJSON = new JSONObject(authString);
             this.token = authJSON.getString("token");
             response.close();
             httpclient.close();
-        } catch (URISyntaxException e) {
+        }		
+
+        catch (URISyntaxException e) {
             throw new RuntimeException(e);
-        } catch (ClientProtocolException e) {
+        } 
+        catch (ClientProtocolException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
